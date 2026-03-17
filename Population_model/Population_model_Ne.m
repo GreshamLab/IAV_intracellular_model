0368b3a45f8a1b39e94be09bb579138f2107da65
es6393 <es6393@nyu.edu>
2026-03-17 18:30:29 +0100

    Refactor simulation script for viral infection model

--- Population_model/Population_model_Ne.m
+++ Population_model/Population_model_Ne.m
@@ -35,10 +35,10 @@ for m = 1:M
     Genomes_name = strcat(current,'/MOI_',num2str(MOIs(m)),'_Genomes',path,'.csv');
     Fit_name = strcat(current,'/MOI_',num2str(MOIs(m)),'_Fit',path,'.csv');
     
-    Genomes = csvread(Genomes_name);
+    Genomes = readmatrix(Genomes_name);
     Genomes_store(:,1:MOIs(m),MOIs(m)) = Genomes;
 
-    Fit = csvread(Fit_name);
+    Fit = readmatrix(Fit_name);
     Fit = Fit(1,:);
     Fit(isnan(Fit)) = 0; Fit(Fit == Inf) = 0;
     Fit_store(:,MOIs(m)) = Fit;
